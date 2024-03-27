1c612d85bb8a38583aa3b09cf986d2159791cc98
mariana <103067658+litzaedit@users.noreply.github.com>
2024-03-27 09:13:22 -0300

    atualizando meu código de novo

--- main.js
+++ main.js
@@ -16,13 +16,12 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-03-01T00:00:00");
-const tempoObjetivo1 = new Date("2025-03-01T00:00:00");
 const tempoObjetivo2 = new Date("2025-03-01T00:00:00");
 const tempoObjetivo3 = new Date("2025-03-01T00:00:00");
-const tempoObjetivo4 = new Date("2025-03-01T00:00:00");
-const tempoObjetivo5 = new Date("2025-03-01T00:00:00");
+const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4,tempoObjetivo5];
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -53,6 +52,4 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-
-comecaCronometro();
 //comecaCronometro();
